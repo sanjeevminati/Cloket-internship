9d558a7ec8a56efe83d927e94d9eacaa561694d4
Hackaman <43167327+sanjeevminati@users.noreply.github.com>
2023-05-22 18:43:39 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        Cloket: "#7867df",
+      },
+      width: {
+        600: "600px", // Replace 'custom' with your desired class name and '300px' with your desired width value
+      },
+      width: {
+        500: "510px",
+      },
+      width: {
+        12: "12px",
+      },
+      width: {
+        200: "202px",
+      },
+    },
+  },
+  plugins: [],
+};
